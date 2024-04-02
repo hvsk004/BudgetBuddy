ab2a7768b5e0de4603bea9a8065bae7c260e06f4
hvsk004 <himanth2004@gmail.com>
2024-04-02 11:46:54 +0530

    Made some changes to db and .env

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,13 @@
 // db.js
 import mongoose from "mongoose";
 
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables from .env file
+
 async function connectDB() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://hvsk:MaZS1QhVMolsAr3n@cluster0.hqql2rd.mongodb.net/"
-    );
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("MongoDB connection error:", error);
