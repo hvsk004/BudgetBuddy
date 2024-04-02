4a1aea03b9a18a1322c860de3a5918495f22f852
hvsk004 <himanth2004@gmail.com>
2024-04-02 11:37:38 +0530

    Added jwtAuthentication in middleware

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,20 +1,31 @@
 import User from "../models/user";
 
+/**
+ * Controller function to handle user login.
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ */
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
+
+    // Find user by email in the database
     const user = await User.findOne({ email });
 
+    // If user not found, return error response
     if (!user) {
       return res.status(400).json({ message: "User not found" });
     }
 
+    // Check if password matches
     if (password !== user.password) {
       return res.status(400).json({ message: "Invalid Password" });
     }
 
+    // If login successful, return success message and user data
     res.status(200).json({ message: "Login Successful", user });
   } catch (error) {
+    // Handle any errors that occur during login process
     console.error("Encountered Error:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
--- server/middlewares/jwtAuthentication.js
+++ server/middlewares/jwtAuthentication.js
@@ -1,1 +1,24 @@
 import jwt from "jsonwebtoken";
+
+import dotenv from "dotenv";
+dotenv.config(); // Load environment variables from .env file
+
+const jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+//Function that creates the jwt
+function signToken(payload) {
+  const token = jwt.sign(payload, jwtSecretKey, { expiresIn: "1w" });
+  return token;
+}
+
+//Function to verify jwt
+function verifyToken(token) {
+  try {
+    const response = jwt.verify(token, jwtSecretKey);
+    return response;
+  } catch (error) {
+    return null;
+  }
+}
+
+module.exports = { signToken, verifyToken };
