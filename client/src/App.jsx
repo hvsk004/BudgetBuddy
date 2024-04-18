3578cf5fe63686859bd93e296f5fee881910acbc
hvsk004 <himanth2004@gmail.com>
2024-04-18 16:00:46 +0530

    Added SideBar Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Sidebar from "./components/Sidebar";
+import piechart from "./assets/pie-chart.png";
+import { SidebarItem } from "./components/Sidebar";
+import dashboardIcon from "./assets/budget.png";
+import logo from "./assets/logo.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const sidebarItems = [
+    {
+      id: 1,
+      icon: dashboardIcon,
+      text: "Dashboard",
+      active: true,
+      alert: false,
+    },
+    {
+      id: 2,
+      icon: piechart,
+      text: "Statistics",
+      active: false,
+      alert: true,
+    },
+  ];
 
   return (
     <>
-      <div>Hello World </div>
+      <Sidebar>
+        <SidebarItem icon={dashboardIcon} text="Dashboard" active alert />
+        <SidebarItem icon={piechart} text="Statistics" active alert />
+      </Sidebar>
     </>
   );
 }
