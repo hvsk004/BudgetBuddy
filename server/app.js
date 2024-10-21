18d632c8812cb364ca0d214fee18e9e609295508
hvsk004 <himanth2004@gmail.com>
2024-10-21 13:39:32 +0530

    Updated server.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 // Initialize Express app
 const app = express();
-
+.
 // Middleware
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -46,7 +46,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
